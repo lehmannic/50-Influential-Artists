127b0ad6e9a3e340035dc7064be5683d763a410b
Nic Lehman <niclehman@Nics-MacBook-Pro.local>
2020-04-17 13:00:11 -0500

    getArtistByIndex completed (JS task 3)

--- index.js
+++ index.js
@@ -208,9 +208,19 @@ const artists = [
 (1) Name of the first artist in the array
 (2) Bio of the third artist in the array */
 
+console.log(artists[0]);  // (1)
+
+console.log(artists[2].bio); // (2)
+
 
 
 /* Task 2: There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
+
+console.log(`${artists[8].name} is name before switch`)
+artists[8].name = "Vincent Van Gogh"; // this is how you make the change
+console.log(artists[8].name);
+
+
 
 
 
@@ -222,9 +232,11 @@ const artists = [
  * For example, if getArtistByIndex is invoked with the inventory and the number 0,
  * it will return `The artist at index 0 is Amedeo Modigliani`.
 */
-function getArtistByIndex(id, name) {
-    /* code here */
+function getArtistByIndex(id, index) {
+    return `The artist at index ${index} is ${id[index].name}`
   }
+
+  console.log(getArtistByIndex(artists, 0)); // this is me testing my code
   
   /**
 
@@ -237,9 +249,14 @@ function getArtistByIndex(id, name) {
  * For example, if removeArtist is invoked with the data and the number 0,
  * it will remove Amedeo Modigliani from our dataset.
 */
-function removeArtist(/*code here*/) {
-    /* code here */
+function removeArtist(someArray, indexToDelete) {
+  someArray = someArray.splice(someArray, indexToDelete); 
+  return someArray; 
   }
+
+  console.log(artists[0]); 
+  removeArtist(artists, 0);
+  console.log(artists[0]);
   
   /**
 
